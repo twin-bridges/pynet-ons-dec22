b3f76d641544fc3b1f534b5ef6ee5b1e7482be42
Kirk Byers <ktbyers@twb-tech.com>
2022-11-30 18:25:06 -0800

    String exercises

--- day1/py_strings/str_ex1.py
+++ day1/py_strings/str_ex1.py
@@ -1,12 +1,10 @@
-#!/usr/bin/env python
-name1 = "Kirk Byers"
-name2 = "George Washington"
-name3 = "Thomas Jefferson"
-name4 = input("\nEnter fourth name: ")
-
 print()
-print(f"{name1:>30}")
-print(f"{name2:>30}")
-print(f"{name3:>30}")
-print(f"{name4:>30}")
+
+name = "Jane Doe"
+print(name)
+print(name.lower())
+print(name.upper())
+print(name.split())
+name = name.lower().capitalize()
+print(name)
 print()
