127ce8b3d8e2157bdc2f4f6d07feddefb773987a
Kirk Byers <ktbyers@twb-tech.com>
2022-12-12 07:31:53 -0800

    Scratch exercise on lists.

---
+++ day2/scratch/lists_ex1.py
@@ -0,0 +1,28 @@
+# Lists Ex1
+# ----------
+# 
+# a. Create a list with five strings
+# 
+# b. Use append to add two strings to the list
+# 
+# c. Use pop to remove the first element
+# 
+# d. Find the length of the list
+# 
+# e. Sort the list
+# 
+# f. Access index-0 (my_list[0]) and assign it a new value.
+
+from rich import print
+
+my_list = ["string1", "something else", "whatever", "hello", "new"]
+
+my_list.append("john")
+my_list.append("doe")
+my_list.pop(0)
+my_list.sort()
+
+my_list[0] = "new string"
+
+print(my_list)
+print(f"The length of our list is: {len(my_list)}")
