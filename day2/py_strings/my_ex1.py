ae806feb70446d655c218967e66919b711f0a4df
Kirk Byers <ktbyers@twb-tech.com>
2022-12-12 13:56:01 -0800

    Aruba AP exercise

--- day2/py_strings/my_ex1.py
+++ day2/py_strings/my_ex1.py
@@ -4,13 +4,19 @@
 
 # In other words, only print out the tabular data from the file. Your output should look as follows:
 
+from rich import print
+
 with open("aruba_show_ap_database.txt") as my_file:
     data = my_file.read()
 
+print(data)
 my_lines = data.splitlines()
 
 header = True
 footer = False
+
+aruba_aps = {}
+
 for element in my_lines:
     if "-------" in element and "     " in element:
         header = False
@@ -23,4 +29,16 @@ for element in my_lines:
         continue
     if not element:
         continue
-    print(element)
+    # print(element)
+
+    fields = element.split()
+    ap_name = fields[0]
+    ip_addr = fields[3]
+    up_down_status = fields[4]
+
+    aruba_aps[ap_name] = up_down_status
+    # print(f"AP Name: {ap_name}")
+    # print(ip_addr)
+    # print(up_down_status)
+
+print(aruba_aps)
