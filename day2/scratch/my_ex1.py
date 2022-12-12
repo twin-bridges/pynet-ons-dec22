e66ae01fa4e94fcf8d4d8456da835934315adaab
Kirk Byers <ktbyers@twb-tech.com>
2022-12-12 14:33:05 -0800

    Day2 dictionary exercise

---
+++ day2/scratch/my_ex1.py
@@ -0,0 +1,28 @@
+#b. Assign it an ip address, a username, a password, a vendor, and a model 
+#   field: ip_addr, username, password, vendor, model.
+from rich import print
+
+chi_rtr1 = {
+    "ip_addr": "10.1.1.1",
+    "username": "admin",
+    "password": "bogus",
+    "vendor": "aruba",
+    "model": "Aruba-CX",
+}
+
+chi_rtr2 = {
+    "ip_addr": "10.1.1.2",
+    "username": "admin",
+    "password": "bogus",
+    "vendor": "aruba",
+    "model": "Aruba-CX",
+}
+
+net_devices = {
+    "chi_rtr1": chi_rtr1,
+    "chi_rtr2": chi_rtr2,
+}
+# net_devices["chi_rtr1"] = chi_rtr1
+# net_devices["chi_rtr2"] = chi_rtr2
+
+print(net_devices)
