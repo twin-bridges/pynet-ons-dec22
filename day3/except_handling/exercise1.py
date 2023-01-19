2dc22ebebb92773eba419029a9d9faa72fb3547e
Kirk Byers <ktbyers@twb-tech.com>
2023-01-18 20:43:30 -0800

    Exception exercise

--- day3/except_handling/exercise1.py
+++ day3/except_handling/exercise1.py
@@ -1,24 +1,10 @@
-def read_file(filename):
-    try:
-        f = open(filename)
-        data = f.read()
-        f.close()
-    except FileNotFoundError:
-        print("\nDarn it...the file was not found\n")
-        data = ""
+filename = "show_versionz.txt"
 
-    return data
-
-
-def find_serial_number(show_ver):
-    serial_number = ""
-    for line in show_ver.splitlines():
-        if "Processor board ID" in line:
-            _, serial_number = line.split("Processor board ID")
-    return serial_number
-
-
-my_file = "show_version.txt"
-show_ver = read_file(my_file)
-serial_number = find_serial_number(show_ver)
-print(f"\nSerial Number is {serial_number}\n")
+try:
+    f = open(filename)
+    data = f.read()
+    f.close()
+    print("\nFile opened successfully\n")
+except FileNotFoundError:
+    print("\nThe file was not found\n")
+    data = ""
--- day3/scratch/test_func.py
+++ day3/scratch/test_func.py
@@ -1,5 +1,6 @@
 def print_hello(arg1):
     print(f"{arg1}")
 
+
 print_hello("hello")
 print("Whatever")
