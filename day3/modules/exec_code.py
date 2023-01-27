a74b7ed9e1de5c6a5710078dc5cfd6b9ea0a5f68
Kirk Byers <ktbyers@twb-tech.com>
2023-01-27 13:14:03 -0800

    Content on functions and modules.

---
+++ day3/modules/exec_code.py
@@ -0,0 +1,7 @@
+import my_module
+
+res = my_module.exponent(5)
+print(res)
+
+res = my_module.product(2, 5)
+print(res)
---
+++ day3/modules/my_module.py
@@ -0,0 +1,36 @@
+#
+# In a new Python file do the following.
+#
+# Create a function that takes a number as input and returns the number squared.
+#
+# Create a second function that takes two numbers and returns the product of those
+# numbers.
+#
+# Use the "if __name__ == "__main__":" technique and separate the functions from the
+# executable code.
+#
+# For the executable code, test your two functions using a couple of test cases. You can use
+# assert statements to verify your test cases work.
+#
+# For example, as a simple test you can do something like the following:
+#
+#    result = squared(2)
+#    assert result == 4
+#
+# Introduce a simple error in your testing (i.e. something that results in the test statements being False)
+# and observe that your testing catches this error).
+
+
+def exponent(x):
+    return x**2
+
+
+def product(y, z):
+    return y * z
+
+
+if __name__ == "__main__":
+    print("Hello world")
+
+    res = exponent(2)
+    assert res == 4
